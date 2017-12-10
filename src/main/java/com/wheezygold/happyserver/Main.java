9b4b678d7c9bf79ed745c47e39f511b3ec13d9b5
WheezyGold7931 <joshroy126@gmail.com>
2017-12-10 11:47:10 -0500

    Get command framework setup

--- src/main/java/com/wheezygold/happyserver/Main.java
+++ src/main/java/com/wheezygold/happyserver/Main.java
@@ -1,1 +1,22 @@
-//hi
+package com.wheezygold.happyserver;
+
+import com.wheezygold.happyserver.util.C;
+import me.jamesj.icommandframework.CommandFramework;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    private static CommandFramework framework;
+
+    @Override
+    public void onEnable() {
+        C.log("Initialized!");
+        C.log("Loading Commands...");
+        loadCommands();
+    }
+
+    private void loadCommands() {
+        framework = new CommandFramework(this);
+    }
+
+}
---
+++ src/main/java/com/wheezygold/happyserver/util/C.java
@@ -0,0 +1,9 @@
+package com.wheezygold.happyserver.util;
+
+public class C {
+
+    public static void log(String message) {
+        System.out.println("[happyserver] " + message);
+    }
+
+}
